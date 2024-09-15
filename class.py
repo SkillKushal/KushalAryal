fb5a8e26a7bc51225bf52184dc0dc7f10eb5ee38
SkillKushal <9843248703k@gmail.com>
2024-09-16 01:05:09 +0545

    asignment

---
+++ class.py
@@ -0,0 +1,58 @@
+# books = [ 
+#     ("The Alchemist", 25),
+#     ("The Da Vinci Code", 30),
+#     ("A Brief History of Time", 15),
+#     ("Angels & Demons", 0),
+#     ("The Grand Design", 0),
+#     ("1984", 19)
+# ]
+
+
+
+
+
+# class Library :
+#     st_count = 0
+
+
+#     def __init__(self,st_name, dept = "CS"):
+#         self.st_name = st_name
+#         self.dept = dept
+#         Library.st_count += 1
+        
+
+#     def book_lend(self,book_name):
+#        print(list3)
+
+#     @classmethod
+#     def get_student_count(cls):
+#         return cls.st_count
+
+# lib1 = Library("Nabin")   
+# print(lib1.st_name) 
+
+
+# lib1.book_lend("1984")
+    
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+        
+        
+
+
+
+
+
+
+
