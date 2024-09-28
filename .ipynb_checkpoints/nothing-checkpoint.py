e8dfcbabe77c81de68eae1575994e30b2f49f74c
Kushal Aryal <akushal.177@gmail.com>
2024-09-28 21:49:59 +0545

    Assignment

---
+++ .ipynb_checkpoints/nothing-checkpoint.py
@@ -0,0 +1,4 @@
+import numpy as np
+import random
+array = np.random.randint(low = 0 , high =10 ,size = (3,3))
+print(array)
--- nothing.py
+++ nothing.py
@@ -1,4 +1,1 @@
-import numpy as np
-import random
-array = np.random.randint(low = 0 , high =10 ,size = (3,3))
-print(array)
+ 
